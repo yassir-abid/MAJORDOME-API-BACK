12890ed05379279e7a78c6771bd3f7f7cc1aa2b9
yassir-abid <abid.yassir@gmail.com>
2022-04-04 18:24:08 +0200

    heroku fixed bugs

--- app/controllers/api/index.js
+++ app/controllers/api/index.js
@@ -4,8 +4,8 @@ const apiController = {
     /**
      * Default API controller to show documention url.
      * ExpressMiddleware signature
-     * @param {object} req Express request object (not used)
-     * @param {object} res Express response object
+     * @param {object} request Express request object (not used)
+     * @param {object} response Express response object
      * @returns {string} Route API JSON response
      */
     home(request, response) {
