0366907a181661559ce425017faa497fc42a523d
yassir-abid <abid.yassir@gmail.com>
2022-04-04 18:11:49 +0200

    debug heroku

--- app/index.js
+++ app/index.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const router = require('./routers');
 
 const app = express();
-require('./helpers/apiDocs')(app);
+// require('./helpers/apiDocs')(app);
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -13,9 +13,11 @@ app.set('view engine', 'pug');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(cors({
+const corsOptions = {
     origin: process.env.CORS_DOMAINS ?? '*',
-}));
+};
+
+app.use(cors(corsOptions));
 
 app.use(router);
 
