9030b1d97b0e1cbdbebefc9ecc163ac1da2283c1
yassir-abid <abid.yassir@gmail.com>
2022-04-13 10:41:42 +0200

    modify create project schema

--- app/validation/schemas/projectCreateSchema.js
+++ app/validation/schemas/projectCreateSchema.js
@@ -2,7 +2,7 @@ const Joi = require('joi');
 
 module.exports = Joi.object({
     title: Joi.string().required(),
-    description: Joi.string(),
-    comments: Joi.string().optional(),
+    description: Joi.string().allow(null, '').optional(),
+    comments: Joi.string().allow(null, '').optional(),
     client_id: Joi.string().required(),
 });
