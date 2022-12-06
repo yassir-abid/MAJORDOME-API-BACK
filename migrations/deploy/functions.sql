9d6219bf547d2ba998cff4a214bc23f2cd91a36d
yassir-abid <abid.yassir@gmail.com>
2022-12-06 14:45:07 +0100

    combine sql function conditions for project status update

--- migrations/deploy/functions.sql
+++ migrations/deploy/functions.sql
@@ -3,14 +3,17 @@
 BEGIN;
 
 CREATE FUNCTION update_project_status(int) RETURNS void AS $$
-	UPDATE project SET status =
-	CASE
-		WHEN EXISTS (SELECT status FROM intervention WHERE project_id = $1 AND status = 'Programmée') THEN 'Ouvert'
-		WHEN EXISTS (SELECT status FROM intervention WHERE project_id = $1 AND status = 'Annulée') THEN 'Clôturé'
-		WHEN EXISTS (SELECT status FROM intervention WHERE project_id = $1 AND status = 'Terminée') THEN 'Clôturé'
-		ELSE 'Vide'
-		END
-	WHERE id = $1;
+    UPDATE project SET status =
+    CASE
+        WHEN 
+            EXISTS (SELECT status FROM intervention WHERE project_id = $1 AND status = 'Programmée') 
+            THEN 'Ouvert'
+        WHEN 
+            EXISTS (SELECT status FROM intervention WHERE project_id = $1 AND (status = 'Annulée' OR status = 'Terminée')) 
+            THEN 'Clôturé'
+        ELSE 'Vide'
+        END
+    WHERE id = $1
 $$LANGUAGE sql STRICT;
 
 
